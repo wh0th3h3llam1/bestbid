a47aada075a6968cc719c6b5a0aa22d6a2c34a95
wh0am1 <>
2020-02-07 14:01:48 +0530

    Used static files for bootstrap

--- bestbid/bidding/urls.py
+++ bestbid/bidding/urls.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import login
 from django.conf.urls.static import static
-from django.conf import settings
+from django.contrib.auth import login
 from django.conf.urls import url
+from django.conf import settings
 from . import views
 
 
